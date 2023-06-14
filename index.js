f09acca23f4ad4cbb47345165d7f543e52128dce
Masud <ranamasud0701@gmail.com>
2023-06-14 18:46:04 +0600

    All toy show

--- index.js
+++ index.js
@@ -39,6 +39,11 @@ async function run() {
         res.send(result);
       })
 
+      app.get('/AllToy',async(req,res)=>{
+         const result = await alltoyCollection.find().toArray();
+         res.send(result); 
+      })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
